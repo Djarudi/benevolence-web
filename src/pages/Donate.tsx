1697f07eeeac57b7408efc725b7bef39938738a3
Djarudi <157922143+Djarudi@users.noreply.github.com>
2026-04-19 20:40:48 +0200

    Update Donate.tsx

--- src/pages/Donate.tsx
+++ src/pages/Donate.tsx
@@ -58,7 +58,8 @@ const Donate = () => (
     </section>
 
     <section className="py-20 bg-card">
-      <a
+      <div className="container">
+        <a
             href="https://donate.benevolenceislove.org/posts/buy-book-for-kid-156008905?utm_medium=clipboard_copy&utm_source=copyLink&utm_campaign=postshare_creator&utm_content=join_link"
             target="_blank"
             rel="noopener noreferrer"
@@ -66,6 +67,7 @@ const Donate = () => (
           >
             Buy A book for one kid!
           </a>
+      <div/>
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 max-w-6xl mx-auto">
           {programs.map((program) => (
